20f738b857ca42e8bb075153e3a20cf64ec92a26
koushith <koushith97@gmail.com>
2021-03-15 13:20:05 +0530

    adds all sterter files

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import Link from '@/components/Link'
+import { PageSeo } from '@/components/SEO'
+import Tag from '@/components/Tag'
+import siteMetadata from '@/data/siteMetadata'
+import { getAllFilesFrontMatter } from '@/lib/mdx'
+import Hero from '../components/Hero'
+
+const MAX_DISPLAY = 5
+const postDateTemplate = { year: 'numeric', month: 'long', day: 'numeric' }
+
+export async function getStaticProps() {
+  const posts = await getAllFilesFrontMatter('blog')
+
+  return { props: { posts } }
+}
+
+export default function Home({ posts }) {
+  return (
+    <>
+    <Hero/>
+    </>
+  )
+}
